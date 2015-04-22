449cdda4764477a4464c6b78318c343ef2c65357
Dima Kovalchuk <kovaldn@gmail.com>
2015-04-22 15:39:20 +0300

    шаг 4 - javascript готов

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,5 @@
+// Для старых браузеров подключаем jquery.placeholder
 if (!Modernizr.input.placeholder){
   $('input, textarea').placeholder();
 } 
-
-
-var addProject = (function (){
-
-	var _init = function(){
-				console.log('Инициализация модуля addProject');
-				_setUpListners();
-			},
-			_setUpListners = function (){
-				$('#add-new-item').on('click', _showModal);
-			},
-			_showModal = function (){
-      	console.log('Вызов модального окна');
-	      $('#new-progect-popup').bPopup({
-	        speed: 650,
-	        transition: 'slideDown'
-	      });
-    	};
-
-	return {
-		init: _init
-	};
-
-})();
-
-addProject.init();
 
