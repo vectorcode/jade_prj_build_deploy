7f56401d984db82a87f06ee0214589ec5d31ed35
Dima Kovalchuk <kovaldn@gmail.com>
2015-04-29 16:41:41 +0300

    jade готов

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp"),
 		connect = require("gulp-connect"),
-		opn = require("opn");
+		opn = require("opn"),
+		jade = require('gulp-jade');
 
 // Запуск сервера c лайврелоадом
 gulp.task('serv_livereload', function() {
@@ -21,11 +22,36 @@ gulp.task('serv_no_livereload', function() {
 	opn('http://localhost:8888');
 });
 
-// Работа с html
-gulp.task('html', function () {
-	gulp.src('./app/*.html')
-	.pipe(connect.reload());
+// Jade 
+gulp.task('templates', function() {
+  gulp.src('./app/templates/pages/*.jade')
+    .pipe(jade({
+			pretty: true
+		 }))
+    .on('error', log)
+    .pipe(gulp.dest('./app/'))
+    .pipe(connect.reload());
 });
+
+function log(error) {
+    console.log([
+        '',
+        "----------ERROR MESSAGE START----------",
+        ("[" + error.name + " in " + error.plugin + "]"),
+        error.message,
+        "----------ERROR MESSAGE END----------",
+        ''
+    ].join('\n'));
+    this.end();
+}
+
+
+
+// Работа с html
+// gulp.task('html', function () {
+// 	gulp.src('./app/*.html')
+// 	.pipe(connect.reload());
+// });
 
 // Работа с css
 gulp.task('css', function () {
@@ -41,7 +67,7 @@ gulp.task('js', function () {
 
 // Слежка
 gulp.task('watch', function () {
-	gulp.watch(['./app/*.html'], ['html']);
+	gulp.watch(['./app/templates/**/*.jade'], ['templates']);
 	gulp.watch(['./app/css/*.css'], ['css']);
 	gulp.watch(['./app/js/*.js'], ['js']);
 });
